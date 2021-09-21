e6b877938925ceba49241a50595f1af7a6df0614
daecheol.choi <daecheol.choi@epapyrus.com>
2021-09-21 16:07:07 +0900

    chore: Initialize project

---
+++ alimee/src/main/java/com/choi/toy/alimee/AlimeeApplication.java
@@ -0,0 +1,13 @@
+package com.choi.toy.alimee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimeeApplication.class, args);
+	}
+
+}
